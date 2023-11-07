196db61cb8c6ffeb510fea935cc06844009fb540
Stefan Psutka <82378971+Senpai021@users.noreply.github.com>
2023-11-07 20:41:47 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -101,12 +101,17 @@ function calc() {
     let celsius = parseFloat(celsiusInput);
     let fahrenheit = parseFloat(fahrenheitInput);
 
-    if(celsius === 69.69){
+    let celsiusInput = document.getElementById("celsius").value;
+    let fahrenheitInput = document.getElementById("fahrenheit").value;
+    let celsius = parseFloat(celsiusInput);
+    let fahrenheit = parseFloat(fahrenheitInput);
+    
+    if (celsius === 69.69 || fahrenheit === 69.69) {
         window.open("https://www.reddit.com/r/Spengergasse/", "_blank");
     }
-
-    if(fahrenheit === 69.69){
-        window.open("https://www.reddit.com/r/Spengergasse/", "_blank");
+    
+    if (celsius === 69.69 || fahrenheit === 69.69) {
+        createYouTubeIframe();
     }
 
     // Check for empty inputs
@@ -134,4 +139,18 @@ function calc() {
         document.getElementById("celsius").value = celsius.toFixed(2);
         alert(`${fahrenheit.toFixed(2)} degrees Fahrenheit is equivalent to ${celsius.toFixed(2)} degrees Celsius`);
     }
+}
+function createYouTubeIframe() {
+    const iframe = document.createElement("iframe");
+    iframe.width = 560;
+    iframe.height = 315;
+    iframe.src = "https://www.youtube.com/embed/PTENmzgkJL8?si=DRUts0r36tKJpGli";
+    iframe.title = "YouTube video player";
+    iframe.frameBorder = 0;
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowFullscreen = true;
+    iframe.style.display = "none";
+
+    // Add the iframe element to the document
+    document.getElementById("iframeContainer").appendChild(iframe);
 }
